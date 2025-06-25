ee2f7110e102c408eb3f2d5372c623feb9689901
raaedmunshi <124057630+raaedmunshi@users.noreply.github.com>
2025-06-26 00:06:28 +0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'Surprise for You:) '.split('')
+const text = 'Just for You :) '.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
