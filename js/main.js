b4caab319fea1a3b987dfc9e42958fcec9f190a2
raaedmunshi <124057630+raaedmunshi@users.noreply.github.com>
2025-06-25 23:58:00 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('Happy Girlfriends Day 💓').split('')
+    const titles = ('Happy Birthday Raghda 💓').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
